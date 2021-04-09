d388d251ae41aa4ec5d59fba62b7d350e50729b1
StephaneBruyere <37586841+StephaneBruyere@users.noreply.github.com>
2021-04-09 14:32:31 +0200

    CakeFactory-init

---
+++ src/test/java/com/factory/cake/CakeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
